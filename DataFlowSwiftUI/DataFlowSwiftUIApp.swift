c8127fe4220099bb5c1126bb91b437de6a1c4dc8
Alexey Kulish <alexkulish1993@gmail.com>
2022-02-23 12:31:35 +0300

    Initial Commit

---
+++ DataFlowSwiftUI/DataFlowSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataFlowSwiftUIApp.swift
+//  DataFlowSwiftUI
+//
+//  Created by Alex Kulish on 23.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct DataFlowSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
