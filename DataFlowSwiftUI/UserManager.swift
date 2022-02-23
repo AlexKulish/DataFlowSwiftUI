31a771f15241e295c0efc7b3460f62cf10e56245
Alexey Kulish <alexkulish1993@gmail.com>
2022-02-23 14:27:24 +0300

    Add UserManager

---
+++ DataFlowSwiftUI/UserManager.swift
@@ -0,0 +1,30 @@
+//
+//  UserManager.swift
+//  DataFlowSwiftUI
+//
+//  Created by Alex Kulish on 23.02.2022.
+//
+
+import Foundation
+import Combine
+
+class UserManager: ObservableObject {
+    
+    @Published var user = User()
+    
+    var nameIsValid: Bool {
+        user.name.count >= 3
+    }
+    
+    init() {}
+    
+    init(user: User) {
+        self.user = user
+    }
+    
+}
+
+class User {
+    var name = ""
+    var isRegister = false
+}
