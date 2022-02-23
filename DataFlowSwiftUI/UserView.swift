160467579589e2b4fce2c701ed7b866f21b2f3ce
Alexey Kulish <alexkulish1993@gmail.com>
2022-02-23 16:38:57 +0300

    Add UserView

---
+++ DataFlowSwiftUI/UserView.swift
@@ -0,0 +1,47 @@
+//
+//  UserView.swift
+//  DataFlowSwiftUI
+//
+//  Created by Alex Kulish on 23.02.2022.
+//
+
+import SwiftUI
+
+struct UserView: View {
+    
+    @EnvironmentObject private var userManager: UserManager
+    @StateObject private var timer = TimeCounter()
+    
+    
+    var body: some View {
+        VStack {
+            Text("Hi, \(userManager.user.name)")
+                .font(.largeTitle)
+                .offset(x: 0, y: 150)
+                .padding()
+            Text("\(timer.counter)")
+                .font(.title)
+                .offset(x: 0, y: 150)
+            
+            Spacer()
+            
+            VStack(spacing: 350) {
+                ButtonView(backgroundColor: .blue, title: "Start") {
+                    timer.startTimer()
+                }
+                ButtonView(backgroundColor: .red, title: "Log Out") {
+                    DataManager.shared.deleteUser(userManager: userManager)
+                }
+            }
+            
+            
+        }
+    }
+}
+
+struct UserView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserView()
+            .environmentObject(UserManager())
+    }
+}
